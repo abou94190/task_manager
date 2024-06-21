2b8fca60cfa3c9d4e51a76b21331f42ef1ca24bf
Abou Ly <166803585+abou94190@users.noreply.github.com>
2024-06-21 12:36:01 +0200

    🐛 FIX: erreur code

--- tests/functional_test_register.py
+++ tests/functional_test_register.py
@@ -9,9 +9,9 @@ import time
 driver = webdriver.Chrome()
 
 
-
 def register(driver, username, password):
-    driver.get('http://localhost:5000/register')  # URL to the registration page
+    # URL to the registration page
+    driver.get('http://localhost:5000/register')
 
     # Wait for the registration page to load
     WebDriverWait(driver, 10).until(
@@ -21,27 +21,21 @@ def register(driver, username, password):
     # Find the input elements by their name attributes
     username_input = driver.find_element(By.NAME, 'username')
     password_input = driver.find_element(By.NAME, 'password')
-    
 
     # Fill in the username and password
     username_input.send_keys("sumon")
     password_input.send_keys("1234")
-    
 
     # Submit the registration form
-    register_button = driver.find_element(By.XPATH,'/html/body/div/form/button')
+    register_button = driver.find_element(
+        By.XPATH, '/html/body/div/form/button')
     register_button.click()
 
+
 def login(Login):
-    Login = driver.find_element(By.XPATH,'/html/body/div/p/a')
+    Login = driver.find_element(By.XPATH, '/html/body/div/p/a')
     Login.click()
-    if Login.click ==driver.get('http://localhost:5000/login'):
+    if Login.click == driver.get('http://localhost:5000/login'):
         print('We can enter Login page successfully')
     else:
         print('we cannot enter Login page successfully')
-
-
-
-
-
-
